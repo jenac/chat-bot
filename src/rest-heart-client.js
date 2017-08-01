3911f44fa896693e3afa79d18dec103e5d035395
Jen Ac <Jen Ac>
2017-08-01 14:15:16 -0500

    wip

--- src/rest-heart-client.js
+++ src/rest-heart-client.js
@@ -1,12 +1,17 @@
-import restHeartConfig from './rest-heart-config';
+import { botConfig } from './bot-config';
 const Client = require('node-rest-client').Client;
 
 class RestHeartClient {
     constructor() {
-        let options_auth = { user: restHeartConfig.userId, password: restHeartConfig.password };
+        let options_auth = { user: botConfig.restHeartUserId, password: botConfig.restHeartPassword };
         this.client = new Client(options_auth);
-        this.messageUrl = `http://${restHeartConfig.server}:${restHeartConfig.port}/${restHeartConfig.database}/messages`;
+        this.handleResponse = function (data, response) {
+            console.log('kkk');
+            console.log(data);
+            console.log(response);
+        }
     }
+
 
     upsertMessage(message) {
         message.id = message.MsgId;
@@ -14,13 +19,7 @@ class RestHeartClient {
             data: message,
             headers: { "Content-Type": "application/json" }
         };
-        this.client.put(`${this.messageUrl}/${message.MsgId}`, args, function (data, response) {
-            console.log(data);
-        });
-    }
-
-    initializeDatabase() {
-        
+        this.client.put(`${botConfig.restHeartUrl}/messages/${message.MsgId}`, args, this.handleResponse);
     }
 }
 
---
+++ src/rest-heart-client.spec.js
@@ -0,0 +1,64 @@
+import { restHeartClient } from './rest-heart-client';
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('rest heart client', () => {
+    it('can upsert message', (done) => {
+        var message = {
+            "MsgId": "177389166873616968",
+            "FromUserName": "@fe7fba99086c2d313375d01cfdb1f45ebb9d89339961890bfb0afadac040fd43",
+            "ToUserName": "@@b0adaadf38472f22a9cd5f3cfc1c691514a0544aec359e1dc2e7996a31c39be4",
+            "MsgType": 1,
+            "Content": "测试asdfasfd一下",
+            "Status": 3,
+            "ImgStatus": 1,
+            "CreateTime": 1500430874,
+            "VoiceLength": 0,
+            "PlayLength": 0,
+            "FileName": "",
+            "FileSize": "",
+            "MediaId": "",
+            "Url": "",
+            "AppMsgType": 0,
+            "StatusNotifyCode": 0,
+            "StatusNotifyUserName": "",
+            "RecommendInfo": {
+                "UserName": "",
+                "NickName": "",
+                "QQNum": 0,
+                "Province": "",
+                "City": "",
+                "Content": "",
+                "Signature": "",
+                "Alias": "",
+                "Scene": 0,
+                "VerifyFlag": 0,
+                "AttrStatus": 0,
+                "Sex": 0,
+                "Ticket": "",
+                "OpCode": 0
+            },
+            "ForwardFlag": 0,
+            "AppInfo": {
+                "AppID": "",
+                "Type": 0
+            },
+            "HasProductId": 0,
+            "Ticket": "",
+            "ImgHeight": 0,
+            "ImgWidth": 0,
+            "SubMsgType": 0,
+            "NewMsgId": 177389166873616960,
+            "OriContent": "",
+            "isSendBySelf": true,
+            "OriginalContent": "测试asdfasdf一下",
+            "level": "info",
+            "message": "",
+            "timestamp": "2017-07-19T02:21:20.476Z"
+        };
+
+        message.id = message.MsgId;
+        restHeartClient.upsertMessage(message);
+        done();
+    });
+})

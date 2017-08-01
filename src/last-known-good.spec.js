b26e8577143ce7651e361b7dee1074f8354ac808
Jen Ac <Jen Ac>
2017-07-31 22:08:50 -0500

    added unit test

--- src/last-known-good.spec.js
+++ src/last-known-good.spec.js
@@ -1,10 +1,10 @@
-import { lastKnownGood } from './last-known-good';
+// import { lastKnownGood } from './last-known-good';
 
-lastKnownGood.saveData({"what": "ok"});
-let data = lastKnownGood.loadData();
-console.log(data);
+// lastKnownGood.saveData({"what": "ok"});
+// let data = lastKnownGood.loadData();
+// console.log(data);
 
-data.foo = "BOOOM";
-lastKnownGood.saveData(data);
-let data2 = lastKnownGood.loadData();
-console.log(data2);
+// data.foo = "BOOOM";
+// lastKnownGood.saveData(data);
+// let data2 = lastKnownGood.loadData();
+// console.log(data2);
--- src/message-send-server.spec.js
+++ src/message-send-server.spec.js
@@ -1,1 +1,35 @@
 import { messageSendServer } from './message-send-server';
+
+
+const chai = require('chai');
+
+var chaiHttp = require('chai-http');
+
+// var mongoose = require("mongoose");
+
+
+
+// var server = require('../server/app');
+
+// var Blob = require("../server/models/blob");
+
+
+
+var should = chai.should;
+const expect = chai.expect;
+const assert = chai.assert;
+
+chai.use(chaiHttp);
+
+describe('message sender server', () => {
+    it('should ok', (done) => {
+        chai.request('http://localhost:3000')
+            .post('/send')
+            .send({id:123, name: 'waht is new'})
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                done();
+            })
+    })
+})

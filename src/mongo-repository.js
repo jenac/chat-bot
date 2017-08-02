9207787901b6fd8540c10d8732cc33239439e43b
Lihe Chen <lihe.chen@gmail.com>
2017-08-02 10:14:59 -0500

    mf

--- src/mongo-repository.js
+++ src/mongo-repository.js
@@ -1,11 +1,19 @@
 export class MongoRepository {
     constructor(mongo) {
         this.messageCollection = mongo.collection('messages');
+        this.contactCollection = mongo.collection('contacts');
     }
 
     upsertMessage(message) {
         message._id = message.MsgId;
         this.messageCollection.save(message, (err, res) => {
+            if (err) throw err;
+        });
+    }
+
+    upsertContact(contact) {
+        contact._id = contact.UserName;
+        this.contactCollection.save(contact, (err, res) => {
             if (err) throw err;
         });
     }
--- src/mongo-repository.spec.js
+++ src/mongo-repository.spec.js
@@ -15,6 +15,7 @@ MongoClient.connect(url, function (err, db) {
 
     describe('mongo repository', () => {
         let mongoRepository = new MongoRepository(db);
+
         it('save message', (done) => {
             var message = {
                 "MsgId": "177389166873616968",
@@ -69,16 +70,64 @@ MongoClient.connect(url, function (err, db) {
                 "timestamp": "2017-07-19T02:21:20.476Z"
             };
 
-            message._id = message.MsgId;
             mongoRepository.upsertMessage(message);
 
-            db.collection('messages').findOne({_id: message.MsgId}, function(err, data) {
+            db.collection('messages').findOne({ _id: message.MsgId }, function (err, data) {
                 assert(err == null);
                 assert(data != null);
                 done();
             });
-            
-        })
+
+        });
+
+        it('save contact', (done) => {
+            let contact = {
+                "Uin": 0,
+                "UserName": "@9fc7cd209e00c66e5ac2ba8bfe71b1bf17bb5612a021dcd7a6c8df0c25c09364",
+                "NickName": "付科",
+                "HeadImgUrl": "/cgi-bin/mmwebwx-bin/webwxgeticon?seq=650690683&username=@9fc7cd209e00c66e5ac2ba8bfe71b1bf17bb5612a021dcd7a6c8df0c25c09364&skey=",
+                "ContactFlag": 3,
+                "MemberCount": 0,
+                "MemberList": [],
+                "RemarkName": "",
+                "HideInputBarFlag": 0,
+                "Sex": 0,
+                "Signature": "知行合一",
+                "VerifyFlag": 0,
+                "OwnerUin": 0,
+                "PYInitial": "FK",
+                "PYQuanPin": "fuke",
+                "RemarkPYInitial": "",
+                "RemarkPYQuanPin": "",
+                "StarFriend": 0,
+                "AppAccountFlag": 0,
+                "Statues": 0,
+                "AttrStatus": 549,
+                "Province": "河北",
+                "City": "沧州",
+                "Alias": "",
+                "SnsFlag": 17,
+                "UniFriend": 0,
+                "DisplayName": "",
+                "ChatRoomId": 0,
+                "KeyWord": "",
+                "EncryChatRoomId": "0",
+                "IsOwner": 0,
+                "OrignalNickName": "付科",
+                "OriginalNickName": "付科",
+                "OrignalRemarkName": "",
+                "OriginalRemarkName": "",
+                "OrignalDisplayName": "",
+                "OriginalDisplayName": "",
+                "isSelf": false
+            };
+            mongoRepository.upsertContact(contact);
+            db.collection('contacts').findOne({ _id: contact.UserName }, function (err, data) {
+                assert(err == null);
+                assert(data != null);
+                done();
+            });
+        });
     })
 });
 

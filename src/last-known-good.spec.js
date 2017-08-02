eb5d5c98a5bcbc4d0c0a7d036b6925ecde0c8de6
Lihe Chen <lihe.chen@gmail.com>
2017-08-02 09:27:31 -0500

    some catchup

--- src/last-known-good.spec.js
+++ src/last-known-good.spec.js
@@ -5,12 +5,16 @@ const assert = chai.assert;
 
 var lkgFile = 'dummy.json';
 describe('Last Know Good', () => {
-    it('save load data', () => {
+    it('save/load/clean data', () => {
         let l1 = new LastKnownGood(lkgFile);
         l1.saveData({foo: "bar"});
 
         let d = l1.loadData();
         console.log(d);
         assert(l1.loadData().foo == "bar");
+
+        l1.cleanup();
+        let fs = require('fs');
+        assert(!fs.existsSync(l1.lkgFile));
     })
 })

27947a935009137785e531815a5ad764ff700b32
Lihe Chen <lihe.chen@gmail.com>
2017-08-08 13:55:12 -0500

    added port in env

--- src/bot-config.js
+++ src/bot-config.js
@@ -2,6 +2,7 @@ class BotConfig {
     constructor () {
         this.dbUrl =  process.env.BOT_DB_URL;
         this.lkgFile = process.env.BOT_LKG_FILE;
+        this.listen = process.env.BOT_LISTEN_PORT;
     }
 }
 export let botConfig = new BotConfig();

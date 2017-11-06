9dfde02efbee4bc5b745d1ad9d309715174e1a2f
Lihe Chen <lihe.chen@gmail.com>
2017-11-06 16:14:04 -0600

    remove markdown docker

--- src/bot-config.js
+++ src/bot-config.js
@@ -8,7 +8,6 @@ export let botConfig = JSON.parse(fs.readFileSync(process.env.BOT_CONFIG_FILE), 
     "lkgFile": "./data/lkg.json",
     "listen": 6688,
     "dataFolder": "./data",
-    "azMdLoggerUrl": "https://botdock-devl.azurewebsites.net/api/MarkdownLogger?code=IbxaGlvdoaAzra2TWWT0Skoabn8SkAd5AeIghEmWwE1xwfjmPgt/UQ==&clientId=default",
     "azRRsiUrl": "https://botdock-devl.azurewebsites.net/api/ReverseRsi?code=hRP0Dsar4NunvQWB56pn1QOnmLqy2lajsahjX7EnRANAn7ZX0Uh8pA==&clientId=default"
 }
 

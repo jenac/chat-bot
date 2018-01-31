abe4be3e21adcec91ad04fbf3e3880cbeadd8a2c
Chen, Lihe <lihe.chen@changehealthcare.com>
2018-01-31 12:20:03 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,95 +1,61 @@
-import { logger } from './logger';
-import { botConfig } from './bot-config';
-import { LastKnownGood } from './last-known-good';
-import { MongoRepository } from './mongo-repository';
-import { MessageStore } from './message-store';
-import { MessageSender } from './message-sender';
-const Wechat = require('wechat4u');
-const qrcode = require('qrcode-terminal');
+'use strict'
+require('babel-register')
+const Wechat = require('wechat4u')
+const qrcode = require('qrcode-terminal')
+const fs = require('fs')
+const request = require('request')
 
-let messageStore;
-//init database
-var MongoClient = require('mongodb').MongoClient;
-let mongoRepository;
-let bot;
-let mongoUrl = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${botConfig.mongoServer}:27017/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`;
-MongoClient.connect(mongoUrl, (err, db) => {
-    if (err) { throw err; }
-    mongoRepository = new MongoRepository(db);
-    messageStore = new MessageStore(mongoRepository, botConfig.dataFolder, bot);
-});
-
-//init restify
-const restify = require('restify');
-var server = restify.createServer();
-server.use(restify.plugins.acceptParser(server.acceptable));
-server.use(restify.plugins.queryParser());
-server.use(restify.plugins.bodyParser());
-
-server.listen(botConfig.listen, () => {
-    console.log('%s listening at %s', server.name, server.url);
-});
-
-let lastKnownGood = new LastKnownGood(botConfig.lkgFile);
-
-var ClientClass = require('node-rest-client').Client;
-const client = new ClientClass();
-
+let bot
 try {
-    bot = new Wechat(lastKnownGood.loadData());
+  bot = new Wechat(require('../sync-data.json'))
 } catch (e) {
-    bot = new Wechat();
+  bot = new Wechat()
 }
 
 if (bot.PROP.uin) {
-    // have last know good, just restart
-    bot.restart();
+  bot.restart()
 } else {
-    bot.start();
+  bot.start()
 }
-let messageSender = new MessageSender(server, bot);
-
 bot.on('uuid', uuid => {
-    qrcode.generate('https://login.weixin.qq.com/l/' + uuid, { small: true });
-    logger.instance.info('QR code linkï¼š', 'https://login.weixin.qq.com/qrcode/' + uuid)
+  qrcode.generate('https://login.weixin.qq.com/l/' + uuid, {
+    small: true
+  })
+  console.log('二维码链接：', 'https://login.weixin.qq.com/qrcode/' + uuid)
 })
 
 bot.on('user-avatar', avatar => {
-    logger.instance.info('User avatar Data URLï¼š', avatar);
+  console.log('登录用户头像Data URL：', avatar)
 })
 
 bot.on('login', () => {
-    logger.instance.info('login successful');
-    //save last known good
-    lastKnownGood.saveData(bot.botData);
+  console.log('登录成功')
+  fs.writeFileSync('./sync-data.json', JSON.stringify(bot.botData))
 })
 
 bot.on('logout', () => {
-    logger.instance.info('logout successful');
-    //clear last known good
-    lastKnownGood.cleanup();
+  console.log('登出成功')
+  fs.unlinkSync('./sync-data.json')
 })
 
 bot.on('contacts-updated', contacts => {
-    for(let c of contacts) {
-        if (c.UserName.startsWith('@@')) {
-            mongoRepository.upsertGroup(c, (err, res) => {
-                if (err) logger.instance.error(err);
-            });
-        } else {
-            mongoRepository.upsertContact(c, (err, res) => {
-                if (err) logger.instance.error(err);
-            });
-        }
-    }
+  let c = contacts.find(c=>c.NickName == '蓝天白云');
+  if (c) {
+    bot.updateRemarkName(c.UserName, 'my test 1');
+    console.log(c);
+  }
+  console.log('联系人数量：', Object.keys(bot.contacts).length)
 })
 
 bot.on('error', err => {
-    logger.instance.error('error', err);
+  console.error('错误：', err)
+})
+
+bot.on('login', () => {
+  let ToUserName = 'filehelper'
 })
 
 bot.on('message', msg => {
-    messageStore.persist(msg);
-    logger.instance.info(msg);
+  console.log(`----------${msg.getDisplayTime()}----------`)
+  console.log(bot.contacts[msg.FromUserName].getDisplayName())
 })
-
